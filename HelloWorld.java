ce4ee0af21e493091e03cc918cc2ddccb547d426
Srikanth Jonnalagadda <201009357+Jonnalas-A@users.noreply.github.com>
2025-04-01 20:59:13 +1100

    Create HelloWorld.java
    Prints Hello! world text to console

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// This line declares a public class named HelloWorld
+public class HelloWorld{
+
+  //This is the main method, the entry point for any Java application.
+  public static void main(String[] args){
+
+    //This line prints the text "Hello! World" to the console. System.out is a standard output stream, and println is a method that prints a line of text.
+    System.out.println("Hello! World");
+
+  }
+}
